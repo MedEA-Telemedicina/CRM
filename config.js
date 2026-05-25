5a8882b33719e435219dff7214d5deee165ccafd
MedEA <testmedeatelemedicina@gmail.com>
2026-05-25 18:48:14 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Inserisci qui la tua chiave API di Google Maps Platform
+// IMPORTANTE: limita la chiave per "HTTP referrers" al tuo dominio GitHub Pages
+// Esempio referrer consentito: https://TUO-UTENTE.github.io/TUO-REPO/*
+const GOOGLE_MAPS_API_KEY = "INSERISCI_LA_TUA_CHIAVE_API";
