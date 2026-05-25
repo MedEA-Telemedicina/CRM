56a12a3152f353c30c53efa986ef4a4755b01683
MedEA <testmedeatelemedicina@gmail.com>
2026-05-25 19:10:21 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -2,3 +2,8 @@
 // IMPORTANTE: limita la chiave per "HTTP referrers" al tuo dominio GitHub Pages
 // Esempio referrer consentito: https://TUO-UTENTE.github.io/TUO-REPO/*
 const GOOGLE_MAPS_API_KEY = "INSERISCI_LA_TUA_CHIAVE_API";
+
+// Centro Italia per "bias" della ricerca Places (migliora i match per attività in Italia)
+const DEFAULT_BIAS_LAT = 41.9028;
+const DEFAULT_BIAS_LNG = 12.4964;
+const DEFAULT_BIAS_RADIUS_METERS = 800000; // 800km
