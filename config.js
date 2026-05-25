fc5609ee91f80a332fe454fe5a4e0dca64dfefc9
MedEA <testmedeatelemedicina@gmail.com>
2026-05-25 19:19:44 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,10 @@
 // Esempio referrer consentito: https://TUO-UTENTE.github.io/TUO-REPO/*
 const GOOGLE_MAPS_API_KEY = "INSERISCI_LA_TUA_CHIAVE_API";
 
-// Centro Italia per "bias" della ricerca Places (migliora i match per attività in Italia)
+// Bias Italia per migliorare i match
 const DEFAULT_BIAS_LAT = 41.9028;
 const DEFAULT_BIAS_LNG = 12.4964;
 const DEFAULT_BIAS_RADIUS_METERS = 800000; // 800km
+
+// Country code di default per trasformare numeri locali (es. 030...) in E.164 (+39...)
+const DEFAULT_COUNTRY_CODE = '39';
